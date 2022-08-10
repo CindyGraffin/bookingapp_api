a5f92887a5569e8d77c127530cc0576e3272746e
CindyGraffin <graffincindy@gmail.com>
2022-08-10 18:04:29 +0200

    feature(api): finish the server with routes and controllers :sparkles:

--- routes/rooms.js
+++ routes/rooms.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import { getRoom, getRooms, createRoom, updateRoom, deleteRoom } from '../controllers/room.js';
+import { verifyAdmin } from '../utils/index.js';
 
 const roomsRouter = express.Router()
 
@@ -6,6 +8,20 @@ roomsRouter.get('/', (req, res) => {
     res.send('rooms')
 })
 
+// GET
+roomsRouter.get('/:id', getRoom);
+
+// GET ALL
+roomsRouter.get('/', getRooms);
+
+// CREATE
+roomsRouter.post('/:hotelid', verifyAdmin, createRoom);
+
+// UPDATE
+roomsRouter.put('/:id', verifyAdmin, updateRoom);
+
+// DELETE
+roomsRouter.delete('/:id/:hotelid', verifyAdmin, deleteRoom);
 
 
 export default roomsRouter;
